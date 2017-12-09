6950ed59ecdca8d2dc9c427b5b96eb663b3406a9
Brian Parkes <parkesbrianj@gmail.com>
2017-12-09 06:50:43 -0600

    created object array for characters

---
+++ assets/javascript/game.js
@@ -0,0 +1,85 @@
+var character = [
+	{
+		'name': 					"Sans", 
+		'health-points': 			,
+		'base-attack-power': 		,
+		'base-hp': 					,
+		'counter-attack-power': 	,
+		'attack-power': 			,
+		'hp': 						,
+		'menu-picture-url': 		"assets/images/menu-sans.jpg",
+		'battle-picture-url': 		"assets/images/battle-sans.gif",
+		'arena-picture-url': 		"assets/images/arena-sans.jpg"
+		'is-user-character': 		false,
+		'is-alive': 				true
+	}
+	{
+		'name': 					"Mario", 
+		'health-points': 			,
+		'base-attack-power': 		,
+		'base-hp': 					,
+		'counter-attack-power': 	,
+		'attack-power': 			,
+		'hp': 						,
+		'menu-picture-url': 		"assets/images/menu-mario.jpg",
+		'battle-picture-url': 		"assets/images/battle-mario.png",
+		'arena-picture-url': 		"assets/images/arena-mario.jpg"
+		'is-user-character': 		false,
+		'is-alive': 				true
+	}
+	{
+		'name': 					"Solid-Snake", 
+		'health-points': 			,
+		'base-attack-power': 		,
+		'base-hp': 					,
+		'counter-attack-power': 	,
+		'attack-power': 			,
+		'hp': 						,
+		'menu-picture-url': 		"assets/images/menu-snake.jpg",
+		'battle-picture-url': 		"assets/images/battle-snake.png",
+		'arena-picture-url': 		"assets/images/arena-snake.jpg"
+		'is-user-character': 		false,
+		'is-alive': 				true
+	}
+	{
+		'name': 					"Brook", 
+		'health-points': 			,
+		'base-attack-power': 		,
+		'base-hp': 					,
+		'counter-attack-power': 	,
+		'attack-power': 			,
+		'hp': 						,
+		'menu-picture-url': 		"assets/images/menu-brook.png",
+		'battle-picture-url': 		"assets/images/battle-brook.jpg",
+		'arena-picture-url': 		"assets/images/arena-brook.jpg"
+		'is-user-character': 		false,
+		'is-alive': 				true
+	}
+	{
+		'name': 					"Robin", 
+		'health-points': 			,
+		'base-attack-power': 		,
+		'base-hp': 					,
+		'counter-attack-power': 	,
+		'attack-power': 			,
+		'hp': 						,
+		'menu-picture-url': 		"assets/images/menu-robin.png",
+		'battle-picture-url': 		"assets/images/battle-robin.webp",
+		'arena-picture-url': 		"assets/images/arena-robin.png"
+		'is-user-character': 		false,
+		'is-alive': 				true
+	}
+	{
+		'name': 					"Kuma", 
+		'health-points': 			,
+		'base-attack-power': 		,
+		'base-hp': 					,
+		'counter-attack-power': 	,
+		'attack-power': 			,
+		'hp': 						,
+		'menu-picture-url': 		"assets/images/menu-kuma.jpg",
+		'battle-picture-url': 		"assets/images/battle-kuma.png",
+		'arena-picture-url': 		"assets/images/arena-kuma.png"
+		'is-user-character': 		false,
+		'is-alive': 				true
+	}
